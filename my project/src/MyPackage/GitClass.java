57f2687adfe2a396bae50bcb4315e6a41680cd35
Deepthy10 <deepthy.velpula@gmail.com>
2017-07-26 15:56:47 -0700

    Update GitClass.java

--- my project/src/MyPackage/GitClass.java
+++ my project/src/MyPackage/GitClass.java
@@ -3,7 +3,7 @@ package MyPackage;
 public class GitClass {
 	public void main (String[] args){
 	System.out.println("Hello!");
-	System.out.println("Hello and Welcome!!");
+	System.out.println("Welcome!!");
 
 }
 }
